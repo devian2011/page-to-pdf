b44c54e196754eade78693bb4d0443e1c052707c
ilya romanov <ilya.romanov@deliver.ru>
2021-11-17 01:17:42 +0300

    Enable chrome headless option

--- application/screenshot.js
+++ application/screenshot.js
@@ -2,24 +2,43 @@ const puppeteer = require("puppeteer");
 
 class screenshot {
 
+    async initBrowser(){
+        this.browser = this.browser || await puppeteer.launch(
+            {
+                headless: true,
+                devtools: false,
+                args: [
+                    '--no-sandbox'
+                ]
+            });
+    }
+
     async pdf(url) {
-        this.browser = this.browser || await puppeteer.launch({headless: false, devtools: false, args: ['--no-sandbox']});
+        await this.initBrowser();
         const page = await this.browser.newPage();
         await page.goto(url, {
             waitUntil: 'networkidle2',
         });
-        await page.emulateMediaType('screen');
-        const file = await page.pdf({format: 'a4'})
+        const file = await page.pdf(
+            {
+                format: 'a4',
+                printBackground: true,
+                margin: {
+                    top: 20,
+                    left: 0,
+                    right: 20,
+                    bottom: 0
+                }
+            })
         await page.close()
 
         return file
     }
 
     async jpg(url) {
-        this.browser = this.browser || await puppeteer.launch({headless: false, devtools: false, args: ['--no-sandbox']});
+        await this.initBrowser();
         const page = await this.browser.newPage();
         await page.goto(url, {waitUntil: 'networkidle2'});
-        await page.emulateMediaType('screen');
         const file = await page.screenshot({type: 'jpeg', fullPage: true})
         await page.close()
 
